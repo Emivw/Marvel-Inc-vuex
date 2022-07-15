7f0737b4a6c454c76aeb9706ceaff7d8b6de1791
Tempestfury11 <Swiftyblue11@gmail.com>
2022-07-15 09:38:55 +0200

    about/admin

---
+++ src/views/About.vue
@@ -0,0 +1,366 @@
+<template>
+  <div class="container py-2">
+    <div class="row"></div>
+  </div>
+  <div class="row p-2">
+    <div class="col-md-4 col-xm-12">
+      <img src="https://i.postimg.cc/T2j1k3Gk/logo.png" alt="logo" />
+    </div>
+    <div class="col-md-8 col-xm-12 mt-5 text-white marge">
+      <div class="col-md-12 py-5">
+        <div class="site-heading text-center">
+          <h2 class="text-white">About <span>Us</span></h2>
+          <h4></h4>
+        </div>
+        <p class="h5 text-white">
+          Marvel Inc was founded in 2022 in Cape Town, South Africa.Four average
+          youngsters showed that big ideas start small and in unlikely places.
+          The founding members came up with the idea while they were enrolled in
+          a brilliant and successful coding academy by the name of Life Choices
+          based in Lansdowne. After they graduated, they came together like the
+          Avengers before them and started a website to sell the items that they
+          gathered around the galaxy. They were so successful that they replaced
+          the Guardians of the Galaxy forcing them to go out of business.
+        </p>
+      </div>
+    </div>
+    <h3 class="display-6 text-white">Meet The Team</h3>
+    <div
+      class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3 marg"
+    >
+      <div class="col">
+        <div class="card h-100 shadow-sm">
+          <div class="text-center">
+            <div class="img-hover-zoom img-hover-zoom--colorize">
+              <img
+                class="shadow"
+                src="https://i.postimg.cc/pTkRxgpj/Super.jpg"
+                alt="Another Image zoom-on-hover effect"
+              />
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="clearfix mb-3"></div>
+
+            <div class="my-2 text-center">
+              <h1>Taucia Jackson</h1>
+            </div>
+            <div class="mb-3">
+              <h2 class="text-uppercase text-center role">team Leader</h2>
+            </div>
+            <div class="box">
+              <div>
+                <ul class="list-inline">
+                  <li class="list-inline-item">
+                    <i class="fab fa-github"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-linkedin-in text-primary"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-instagram text-danger"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-twitter text-primary"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 shadow-sm">
+          <div class="text-center">
+            <div class="img-hover-zoom img-hover-zoom--colorize">
+              <img
+                class="shadow"
+                src="https://i.postimg.cc/y6GZB9md/LS2.jpg"
+                alt="Another Image zoom-on-hover effect"
+              />
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="clearfix mb-3"></div>
+
+            <div class="my-2 text-center">
+              <h1>Mr Jankowski</h1>
+            </div>
+            <div class="mb-3">
+              <h2 class="text-uppercase text-center role">Content Manager</h2>
+            </div>
+            <div class="box">
+              <div>
+                <ul class="list-inline">
+                  <li class="list-inline-item">
+                    <i class="fab fa-github"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-linkedin-in text-primary"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-instagram text-danger"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-twitter text-primary"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 shadow-sm">
+          <div class="text-center">
+            <div class="img-hover-zoom img-hover-zoom--colorize">
+              <img
+                class="shadow"
+                src="https://i.postimg.cc/7hpvYX9w/Emile.jpg"
+                alt="Another Image zoom-on-hover effect"
+              />
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="clearfix mb-3"></div>
+
+            <div class="my-2 text-center">
+              <h1>Emile Van Wyk</h1>
+            </div>
+            <div class="mb-3">
+              <h2 class="text-uppercase text-center role">
+                Full-Stacks Developer / Project Head of Developement
+              </h2>
+            </div>
+            <div class="box">
+              <div>
+                <ul class="list-inline">
+                  <li class="list-inline-item">
+                    <i class="fab fa-github"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-linkedin-in text-primary"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-instagram text-danger"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-twitter text-primary"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 shadow-sm">
+          <div class="text-center">
+            <div class="img-hover-zoom img-hover-zoom--colorize">
+              <img
+                class="shadow"
+                src="https://i.postimg.cc/W1tv6FDB/Daniel.jpg"
+                alt="Another Image zoom-on-hover effect"
+              />
+            </div>
+          </div>
+
+          <div class="card-body">
+            <div class="clearfix mb-3"></div>
+
+            <div class="my-2 text-center">
+              <h1>Daniel Fredricks</h1>
+            </div>
+            <div class="mb-3">
+              <h2 class="text-uppercase text-center role">
+                Back-End Developer
+              </h2>
+            </div>
+            <div class="box">
+              <div>
+                <ul class="list-inline">
+                  <li class="list-inline-item">
+                    <i class="fab fa-github"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-linkedin-in text-primary"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-instagram text-danger"></i>
+                  </li>
+                  <li class="list-inline-item">
+                    <i class="fab fa-twitter text-primary"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+@import url("https://use.fontawesome.com/releases/v5.13.0/css/all.css");
+:root {
+  --font3: "Roboto", sans-serif;
+}
+.marg {
+  margin-bottom: 10rem;
+}
+.marge {
+  margin-bottom: 12rem;
+}
+
+body {
+  overflow-x: hidden;
+  background-color: #f8002f;
+  background-image: linear-gradient(326deg, #f8002f 0%, #000c14 74%);
+}
+
+.text-aqua {
+  color: aqua;
+}
+
+.h1,
+h1 {
+  font-size: 1.5rem;
+}
+
+.container-fluid {
+  max-width: 1400px;
+}
+
+.card {
+  background: #fff;
+  box-shadow: 0 6px 10px rgba(0, 0, 0, 0.08), 0 0 6px rgba(0, 0, 0, 0.05);
+  border: 1;
+  border-radius: 1rem;
+}
+
+.img-hover-zoom--colorize img {
+  border-radius: 50%;
+  width: 150px;
+  height: auto;
+  margin-top: 20px;
+  padding: 1px;
+  transition: transform 0.5s;
+  filter: grayscale(100%);
+}
+
+.img-hover-zoom--colorize:hover img {
+  filter: grayscale(0);
+  transform: scale(1.05);
+}
+
+.card h5 {
+  overflow: hidden;
+  height: 80px;
+  font-weight: 300;
+  font-size: 1rem;
+}
+
+.card h5 a {
+  color: black;
+  text-decoration: none;
+}
+
+.role {
+  color: #7a7a7a;
+}
+
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.fab {
+  font-size: 1.5rem;
+  color: black;
+  transition: transform 0.5s;
+}
+
+.fab:hover {
+  color: black;
+  transform: scale(1.1);
+}
+
+.card h2 {
+  font-size: 1rem;
+}
+
+/* MEDIA */
+
+@media only screen and (min-width: 1200px) {
+  .img-hover-zoom--colorize img {
+    width: 200px;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  .h1,
+  h1 {
+    font-size: 2rem;
+  }
+}
+/* for headers */
+section {
+  padding: 60px 0;
+  min-height: 100vh;
+}
+a,
+a:hover,
+a:focus,
+a:active {
+  text-decoration: none;
+  outline: none;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.bg-gray {
+  background-color: #f9f9f9;
+}
+
+.site-heading h2 {
+  display: block;
+  font-weight: 700;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+}
+
+.site-heading h2 span {
+  color: #a00000;
+}
+
+.site-heading h4 {
+  display: inline-block;
+  padding-bottom: 20px;
+  position: relative;
+  text-transform: capitalize;
+  z-index: 1;
+}
+
+.site-heading h4::before {
+  background: #a00000 none repeat scroll 0 0;
+  bottom: 0;
+  content: "";
+  height: 2px;
+  left: 50%;
+  margin-left: -25px;
+  position: absolute;
+  width: 50px;
+}
+</style>
---
+++ src/views/Admin.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="container-fluid table-responsive" id="grad">
+
+    <div class="site-heading text-center">
+        <h2 class="text-white">Admin <span>Page</span></h2>
+        <h4></h4>
+      </div>
+
+    <div class="row">
+      <nav class="d-flex flex-row justify-content-between">
+        <button type="button" id="addThis" class="btn btn"><router-link to="/">Home</router-link></button>
+        <button type="button" class="btn" id="addThis" data-bs-toggle="modal" data-bs-target="#exampleModal">Add</button>
+      </nav>
+    </div>
+  <div class="container table-responsive py-5 text-white">
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <label for="name"></label>
+        <input type="text" name="" id="add-Name" placeholder="Enter Name" required>
+        <label for="img"></label>
+        <input type="img" name="" id="add-Img" placeholder="Add Image" required>
+        <label for="desc"></label>
+        <input type="text" name="" id="add-Desc" placeholder="Add Description" required>
+        <label for="category"></label>
+        <input type="text" name="" id="add-Category" placeholder="Add Category Class" required>
+        <label for="price"></label>
+        <input type="number" name="" id="add-Price" placeholder="Add Price Value" required>
+        <label for="power"></label>
+        <input type="number" name="" id="add-Power" placeholder="Add Power Level" required>
+        <label for="usedby"></label>
+        <input type="text" name="" id="add-Usedby" placeholder="Add User" required>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button @click="$store.commit('addProduct')" type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <table class="table table-bordered text-white">
+      <thead class="thead-dark text-white">
+        <tr class="">
+          <th scope="col">id</th>
+          <th scope="col">title</th>
+          <th scope="col">img</th>
+          <th scope="col">desc</th>
+          <th scope="col">category</th>
+          <th scope="col">price</th>
+          <th scope="col">power</th>
+          <th scope="col">usedby</th>
+          <th scope="col">actions</th>
+        </tr>
+      </thead>
+
+      <tbody >
+        <tr v-for="product in products" 
+    :key="product.id" 
+    :product="product" class="">
+          <th scope="row">{{ product.id }}</th>
+          <td>{{ product.title }}</td>
+          <td><img :src="product.img" alt=""></td>
+          <td>{{ product.desc }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.power }}</td>
+          <td>{{ product["used_by"] }}</td>
+          <td><i class="bi bi-trash-fill" @click="$store.commit('deleteProduct',product.id)">Remove Item</i></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+   </div>
+
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+export default {
+    
+ mounted() {
+    return this.$store.dispatch("getProducts");
+  },
+  computed: {
+    products() {
+      return this.$store.state.products
+    },
+  },
+  components: { Modal },
+};
+</script>
+
+<style>
+li {
+  list-style: none;
+}
+#grad{ 
+    background-color: #F8002F;
+background-image: linear-gradient(326deg, #F8002F 0%, #000C14 74%);
+}
+#addThis{
+  text-decoration: underline;
+  color:#a00000;
+}
+#addThis:hover{
+color: white;
+}
+nav a{
+  color:#a00000;
+
+}
+a:hover{
+  color: white;}
+  .site-heading h2 {
+  display: block;
+  font-weight: 700;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+}
+
+.site-heading h2 span {
+  color: #a00000;
+}
+
+.site-heading h4 {
+  display: inline-block;
+  padding-bottom: 20px;
+  position: relative;
+  text-transform: capitalize;
+  z-index: 1;
+}
+
+.site-heading h4::before {
+  background: #a00000 none repeat scroll 0 0;
+  bottom: 0;
+  content: "";
+  height: 2px;
+  left: 50%;
+  margin-left: -25px;
+  position: absolute;
+  width: 50px;
+}
+
+td{
+  color:white;
+}
+         
+         .btn-grad {
+            background-image: linear-gradient(to right, #000000 0%, #434343  51%, #000000  100%);
+            margin: 10px;
+            padding: 15px 45px;
+            text-align: center;
+            text-transform: uppercase;
+            transition: 0.5s;
+            background-size: 200% auto;
+            color: white;            
+            box-shadow: 0 0 20px #eee;
+            border-radius: 10px;
+            display: block;
+          }
+
+          .btn-grad:hover {
+            background-position: right center; /* change the direction of the change here */
+            color: #fff;
+            text-decoration: none;
+          }
+         
+</style>
